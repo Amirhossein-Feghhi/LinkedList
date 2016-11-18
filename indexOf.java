62fe344724e20f569bd92acc8d2ecebd95fe730f
Amirhossein <a.h.f619@gmail.com>
2016-11-18 18:29:05 +0330

    Added indexOf.java

---
+++ indexOf.java
@@ -0,0 +1,16 @@
+//author Amirhossein
+
+public int indexOf(int element){
+	Node index = head;
+	int count = 0;
+	while(index != null){
+		if(index.data == element){
+			return count;
+		}
+		else{
+			index = index.next;
+			count++;
+		}
+	}
+	return -1;
+}
